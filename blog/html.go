5ca44b4927d5ff15c4acf719fa46ed0b11f5ae93
Egon Elbre <egonelbre@gmail.com>
2020-06-12 17:38:31 +0300

    blog: clarify render

--- blog/html.go
+++ blog/html.go
@@ -9,15 +9,19 @@ import (
 )
 
 func (blog *Blog) WriteToDir(output string) error {
+	render := &Render{
+		Blog: blog,
+	}
+
 	for _, page := range blog.Pages {
-		html := RenderPageHTML(blog, page)
+		html := render.Page(page)
 		err := WritePageFile(output, page.Path(), html)
 		if err != nil {
 			return fmt.Errorf("failed to write page %q: %w", page.Slug, err)
 		}
 	}
 
-	html := RenderTableOfContentHTML(blog)
+	html := render.TableOfContent()
 	err := WritePageFile(output, "index.html", html)
 	if err != nil {
 		return fmt.Errorf("failed to table of content: %w", err)
@@ -35,7 +39,54 @@ func WritePageFile(dir, local string, html string) error {
 	return nil
 }
 
-func RenderHTML(title string, body func(*strings.Builder)) string {
+type Render struct {
+	Blog *Blog
+}
+
+func (render *Render) Page(page *Page) string {
+	return render.HTML(page.Title, func(s *strings.Builder) {
+		render.Nav(s)
+
+		s.WriteString("<h1>")
+		s.WriteString(html.EscapeString(page.Title))
+		s.WriteString("</h1>")
+
+		for _, paragraph := range page.Paragraphs {
+			s.WriteString("<p>")
+			s.WriteString(html.EscapeString(paragraph))
+			s.WriteString("</p>")
+		}
+	})
+}
+
+func (render *Render) TableOfContent() string {
+	return render.HTML(render.Blog.Title, func(s *strings.Builder) {
+		s.WriteString("<h1>")
+		s.WriteString(html.EscapeString(render.Blog.Title))
+		s.WriteString("</h1>")
+
+		render.Nav(s)
+	})
+}
+
+func (render *Render) Nav(s *strings.Builder) {
+	s.WriteString("<ul>")
+	defer s.WriteString("</ul>")
+
+	s.WriteString("<li>")
+	s.WriteString("<a href='index.html'>Table of Content</a>")
+	s.WriteString("</li>")
+
+	for _, page := range render.Blog.Pages {
+		s.WriteString("<li>")
+		s.WriteString("<a href='" + page.Path() + "'>")
+		s.WriteString(html.EscapeString(page.Title))
+		s.WriteString("</a>")
+		s.WriteString("</li>")
+	}
+}
+
+func (render *Render) HTML(title string, body func(*strings.Builder)) string {
 	var s strings.Builder
 	(func() {
 		s.WriteString(`<!DOCTYPE html>`)
@@ -58,47 +109,4 @@ func RenderHTML(title string, body func(*strings.Builder)) string {
 	})()
 
 	return s.String()
-}
-
-func RenderPageHTML(blog *Blog, page *Page) string {
-	return RenderHTML(page.Title, func(s *strings.Builder) {
-		RenderNav(blog, s)
-
-		s.WriteString("<h1>")
-		s.WriteString(html.EscapeString(page.Title))
-		s.WriteString("</h1>")
-
-		for _, paragraph := range page.Paragraphs {
-			s.WriteString("<p>")
-			s.WriteString(html.EscapeString(paragraph))
-			s.WriteString("</p>")
-		}
-	})
-}
-
-func RenderTableOfContentHTML(blog *Blog) string {
-	return RenderHTML(blog.Title, func(s *strings.Builder) {
-		s.WriteString("<h1>")
-		s.WriteString(html.EscapeString(blog.Title))
-		s.WriteString("</h1>")
-
-		RenderNav(blog, s)
-	})
-}
-
-func RenderNav(blog *Blog, s *strings.Builder) {
-	s.WriteString("<ul>")
-	defer s.WriteString("</ul>")
-
-	s.WriteString("<li>")
-	s.WriteString("<a href='index.html'>Table of Content</a>")
-	s.WriteString("</li>")
-
-	for _, page := range blog.Pages {
-		s.WriteString("<li>")
-		s.WriteString("<a href='" + page.Path() + "'>")
-		s.WriteString(html.EscapeString(page.Title))
-		s.WriteString("</a>")
-		s.WriteString("</li>")
-	}
 }
